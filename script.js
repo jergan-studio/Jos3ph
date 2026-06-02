33c6aa8aff1943c910cc46fb53636a3f095fc337
jergan-studio <jose343lopez22@gmail.com>
2026-06-02 16:46:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let currentUser = null;
 
-function log(msg) {
+function log(msg){
   const c = document.getElementById("console");
   c.innerHTML += msg + "<br>";
   c.scrollTop = c.scrollHeight;
 }
 
-function login() {
+function login(){
   currentUser = document.getElementById("user").value;
-  log("Logged in as " + currentUser);
+  log("Logged in: " + currentUser);
 }
 
 /* =========================
@@ -28,18 +28,18 @@ let extensions = {};
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-function draw() {
-  ctx.clearRect(0, 0, 300, 300);
+function draw(){
+  ctx.clearRect(0,0,300,300);
 
-  for (let s in sprites) {
+  for(let s in sprites){
     const sp = sprites[s];
 
     ctx.save();
-    ctx.translate(sp.x + 15, sp.y + 15);
-    ctx.rotate((sp.rot || 0) * Math.PI / 180);
+    ctx.translate(sp.x+15, sp.y+15);
+    ctx.rotate((sp.rot||0)*Math.PI/180);
 
     ctx.fillStyle = sp.color;
-    ctx.fillRect(-15, -15, 30, 30);
+    ctx.fillRect(-15,-15,30,30);
 
     ctx.restore();
   }
@@ -49,23 +49,17 @@ function draw() {
    BLOCKLY INIT
 ========================= */
 
-const workspace = Blockly.inject("blocklyDiv", {
+const workspace = Blockly.inject("blocklyDiv",{
   toolbox: document.getElementById("toolbox")
 });
 
 /* =========================
-   RUN ENGINE
-========================= */
-
-async function run() {
-  await eval(`(async()=>{${Blockly.JavaScript.workspaceToCode(workspace)}})()`);
-}
-
-/* =========================
-   SPRITES
+   🔥 FIX: ALL BLOCKS IN ONE PLACE
 ========================= */
 
 Blockly.defineBlocksWithJsonArray([
+
+/* ===== SPRITES ===== */
 {
   type:"add_sprite",
   message0:"add sprite %1",
@@ -73,30 +67,16 @@ Blockly.defineBlocksWithJsonArray([
   previousStatement:null,
   nextStatement:null,
   colour:210
-}]);
-
-Blockly.JavaScript.forBlock.add_sprite = b => {
-  const n = b.getFieldValue("NAME");
-  return `
-sprites["${n}"]={x:100,y:100,color:"#ff8800",rot:0};
-currentSprite="${n}";
-draw();
-`;
-};
-
-Blockly.defineBlocksWithJsonArray([{
+},
+{
   type:"delete_sprite",
   message0:"delete sprite %1",
   args0:[{type:"field_input",name:"NAME",text:"sprite1"}],
   previousStatement:null,
   nextStatement:null,
   colour:210
-}]);
-
-Blockly.JavaScript.forBlock.delete_sprite = b =>
-  `delete sprites["${b.getFieldValue("NAME")}"]; draw();`;
-
-Blockly.defineBlocksWithJsonArray([{
+},
+{
   type:"select_sprite",
   message0:"select sprite %1",
   args0:[{
@@ -107,55 +87,33 @@ Blockly.defineBlocksWithJsonArray([{
   previousStatement:null,
   nextStatement:null,
   colour:210
-}]);
-
-Blockly.JavaScript.forBlock.select_sprite = b =>
-  `currentSprite="${b.getFieldValue("SPRITE")}";`;
-
-/* MOVE */
-Blockly.defineBlocksWithJsonArray([{
+},
+{
   type:"move",
   message0:"move %1 steps",
   args0:[{type:"field_number",name:"STEPS",value:10}],
   previousStatement:null,
   nextStatement:null,
   colour:210
-}]);
-
-Blockly.JavaScript.forBlock.move =
-  b => `sprites[currentSprite].x+=${b.getFieldValue("STEPS")};draw();`;
-
-/* COLOR */
-Blockly.defineBlocksWithJsonArray([{
+},
+{
   type:"set_color",
   message0:"set color %1",
   args0:[{type:"field_colour",name:"COLOR",colour:"#ff8800"}],
   previousStatement:null,
   nextStatement:null,
   colour:210
-}]);
-
-Blockly.JavaScript.forBlock.set_color =
-  b => `sprites[currentSprite].color="${b.getFieldValue("COLOR")}";draw();`;
-
-/* SPIN */
-Blockly.defineBlocksWithJsonArray([{
+},
+{
   type:"spin",
-  message0:"spin %1",
+  message0:"spin %1 degrees",
   args0:[{type:"field_number",name:"DEG",value:15}],
   previousStatement:null,
   nextStatement:null,
-  colour:120
-}]);
+  colour:210
+},
 
-Blockly.JavaScript.forBlock.spin =
-  b => `sprites[currentSprite].rot+=${b.getFieldValue("DEG")};draw();`;
-
-/* =========================
-   VARIABLES
-========================= */
-
-Blockly.defineBlocksWithJsonArray([
+/* ===== VARIABLES ===== */
 {
   type:"set_var",
   message0:"set %1 to %2",
@@ -184,23 +142,9 @@ Blockly.defineBlocksWithJsonArray([
   args0:[{type:"field_input",name:"NAME"}],
   output:"Number",
   colour:330
-}
-]);
+},
 
-Blockly.JavaScript.forBlock.set_var =
-  b => `variables["${b.getFieldValue("NAME")}"]=${b.getFieldValue("VALUE")};`;
-
-Blockly.JavaScript.forBlock.change_var =
-  b => `variables["${b.getFieldValue("NAME")}"]=(variables["${b.getFieldValue("NAME")}"]||0)+${b.getFieldValue("VALUE")};`;
-
-Blockly.JavaScript.forBlock.get_var =
-  b => [`variables["${b.getFieldValue("NAME")}"]||0`,0];
-
-/* =========================
-   FUNCTIONS
-========================= */
-
-Blockly.defineBlocksWithJsonArray([
+/* ===== FUNCTIONS ===== */
 {
   type:"function_def",
   message0:"function %1 %2",
@@ -217,27 +161,12 @@ Blockly.defineBlocksWithJsonArray([
   previousStatement:null,
   nextStatement:null,
   colour:200
-}
-]);
+},
 
-Blockly.JavaScript.forBlock.function_def = b => {
-  const n = b.getFieldValue("NAME");
-  const body = Blockly.JavaScript.statementToCode(b,"BODY");
-  functions[n]=body;
-  return `function ${n}(){${body}}`;
-};
-
-Blockly.JavaScript.forBlock.function_call =
-  b => `${b.getFieldValue("NAME")}();`;
-
-/* =========================
-   LOOPS
-========================= */
-
-Blockly.defineBlocksWithJsonArray([
+/* ===== LOOPS ===== */
 {
   type:"repeat",
-  message0:"repeat %1",
+  message0:"repeat %1 times %2",
   args0:[
     {type:"field_number",name:"COUNT",value:5},
     {type:"input_statement",name:"DO"}
@@ -245,15 +174,7 @@ Blockly.defineBlocksWithJsonArray([
   previousStatement:null,
   nextStatement:null,
   colour:60
-}
-]);
-
-Blockly.JavaScript.forBlock.repeat = b => {
-  const code = Blockly.JavaScript.statementToCode(b,"DO");
-  return `for(let i=0;i<${b.getFieldValue("COUNT")};i++){${code}}`;
-};
-
-Blockly.defineBlocksWithJsonArray([
+},
 {
   type:"forever",
   message0:"forever %1",
@@ -261,28 +182,82 @@ Blockly.defineBlocksWithJsonArray([
   previousStatement:null,
   nextStatement:null,
   colour:60
-}
-]);
+},
 
-Blockly.JavaScript.forBlock.forever = b => {
-  const code = Blockly.JavaScript.statementToCode(b,"DO");
-  return `while(true){${code} await new Promise(r=>setTimeout(r,0));}`;
-};
-
-/* =========================
-   SOUND
-========================= */
-
-Blockly.defineBlocksWithJsonArray([{
+/* ===== SOUND ===== */
+{
   type:"play_sound",
   message0:"play sound",
   previousStatement:null,
   nextStatement:null,
   colour:290
-}]);
+}
 
-Blockly.JavaScript.forBlock.play_sound =
-  () => `
+]);
+
+/* =========================
+   GENERATORS
+========================= */
+
+/* SPRITES */
+Blockly.JavaScript.forBlock.add_sprite = b=>{
+  const n=b.getFieldValue("NAME");
+  return `
+sprites["${n}"]={x:100,y:100,color:"#ff8800",rot:0};
+currentSprite="${n}";
+draw();
+`;
+};
+
+Blockly.JavaScript.forBlock.delete_sprite = b=>
+  `delete sprites["${b.getFieldValue("NAME")}"]; draw();`;
+
+Blockly.JavaScript.forBlock.select_sprite = b=>
+  `currentSprite="${b.getFieldValue("SPRITE")}";`;
+
+Blockly.JavaScript.forBlock.move =
+  b=>`sprites[currentSprite].x+=${b.getFieldValue("STEPS")};draw();`;
+
+Blockly.JavaScript.forBlock.set_color =
+  b=>`sprites[currentSprite].color="${b.getFieldValue("COLOR")}";draw();`;
+
+Blockly.JavaScript.forBlock.spin =
+  b=>`sprites[currentSprite].rot+=${b.getFieldValue("DEG")};draw();`;
+
+/* VARIABLES */
+Blockly.JavaScript.forBlock.set_var =
+  b=>`variables["${b.getFieldValue("NAME")}"]=${b.getFieldValue("VALUE")};`;
+
+Blockly.JavaScript.forBlock.change_var =
+  b=>`variables["${b.getFieldValue("NAME")}"]=(variables["${b.getFieldValue("NAME")}"]||0)+${b.getFieldValue("VALUE")};`;
+
+Blockly.JavaScript.forBlock.get_var =
+  b=>[`variables["${b.getFieldValue("NAME")}"]||0`,0];
+
+/* FUNCTIONS */
+Blockly.JavaScript.forBlock.function_def = b=>{
+  const n=b.getFieldValue("NAME");
+  const body=Blockly.JavaScript.statementToCode(b,"BODY");
+  functions[n]=body;
+  return `function ${n}(){${body}}`;
+};
+
+Blockly.JavaScript.forBlock.function_call =
+  b=>`${b.getFieldValue("NAME")}();`;
+
+/* LOOPS */
+Blockly.JavaScript.forBlock.repeat = b=>{
+  const code=Blockly.JavaScript.statementToCode(b,"DO");
+  return `for(let i=0;i<${b.getFieldValue("COUNT")};i++){${code}}`;
+};
+
+Blockly.JavaScript.forBlock.forever = b=>{
+  const code=Blockly.JavaScript.statementToCode(b,"DO");
+  return `while(true){${code}}`;
+};
+
+/* SOUND */
+Blockly.JavaScript.forBlock.play_sound = ()=>`
 const a=new AudioContext();
 const o=a.createOscillator();
 o.connect(a.destination);
@@ -291,31 +266,16 @@ o.stop(a.currentTime+0.2);
 `;
 
 /* =========================
-   SAVE / SHARE
+   RUN ENGINE (FIXED)
 ========================= */
 
-function saveCloud(){
-  if(!currentUser) return log("login first");
-  const xml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-  localStorage.setItem("jos3ph_"+currentUser, xml);
-  log("saved");
+function run(){
+  const code = Blockly.JavaScript.workspaceToCode(workspace);
+  eval(code);
 }
 
-function share(){
-  const xml = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
-  navigator.clipboard.writeText(location.origin+"?p="+btoa(xml));
-  log("share copied");
-}
+/* =========================
+   INIT
+========================= */
 
-function exportJS(){
-  log(Blockly.JavaScript.workspaceToCode(workspace));
-}
-
-/* LOAD SHARE */
-const p = new URLSearchParams(location.search).get("p");
-if(p){
-  Blockly.Xml.domToWorkspace(
-    Blockly.Xml.textToDom(atob(p)),
-    workspace
-  );
-}
+draw();
